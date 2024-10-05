169bca2bd50f7994bc0e88cd0dcc44afbfc4ab99
crisilinfotech1 <manager.crisil@gmail.com>
2024-10-05 11:21:15 +0530

    deepak done

---
+++ include/js/custome.js
@@ -0,0 +1,35 @@
+$('.owl-1').owlCarousel({
+    loop:true,
+    margin:50,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+
+$('.owl-2').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
